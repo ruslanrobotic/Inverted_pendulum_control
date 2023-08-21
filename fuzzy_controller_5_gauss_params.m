7277bd65b201a7d6f35ac450f1f7036b1e1707a7
Ruslan Kolesnichenko <83371383+ruslanrobotic@users.noreply.github.com>
2023-08-21 23:42:57 +0500

    Added model, vars files

---
+++ fuzzy_controller_5_gauss_params.m
@@ -0,0 +1,39 @@
+% параметры функций принадлежности входной переменной fi
+fi_1_sigma = 0.1502;
+fi_1_mu = -0.7664;
+fi_2_sigma = 0.1669;
+fi_2_mu = -0.393;
+fi_3_sigma = 0.1669;
+fi_3_mu = 0;
+fi_4_sigma = 0.1669;
+fi_4_mu = 0.393;
+fi_5_sigma = 0.1502;
+fi_5_mu = 0.7664;
+
+% параметры функций принадлежности входной переменной omega
+omega_1_sigma = 0.3;
+omega_1_mu = -1.531;
+omega_2_sigma = 0.3334;
+omega_2_mu = -0.785;
+omega_3_sigma = 0.3334;
+omega_3_mu = 0;
+omega_4_sigma = 0.3334;
+omega_4_mu = 0.785;
+omega_5_sigma = 0.3;
+omega_5_mu = 1.531;
+
+% диапазон изменения выходной переменной force
+FORCE_Range_1 = -150;
+FORCE_Range_2 = 150;
+
+% параметры функций принадлежности выходной переменной force
+force_1_sigma = 28.66;
+force_1_mu = -146.3;
+force_2_sigma = 31.85;
+force_2_mu = -75;
+force_3_sigma = 31.85;
+force_3_mu = 0;
+force_4_sigma = 31.85;
+force_4_mu = 75;
+force_5_sigma = 28.66;
+force_5_mu = 146.3;
